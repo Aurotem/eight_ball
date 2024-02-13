bb829f16d8c651fa377349725f90242c9ed9ed34
Melihcan Fidan <melihcan.fi@gmail.com>
2024-02-13 13:50:18 +0300

    Project start

---
+++ README.md
@@ -0,0 +1,5 @@
+# 8 Ball
+---
+## An 8 ball project that makes no sense.
+
+*Made with flutter.*
---
+++ android/app/src/main/kotlin/com/aurotem/eight_ball/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aurotem.eight_ball
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
